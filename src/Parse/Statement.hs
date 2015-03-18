6f6febe12f2fecbc2e0cc872942acf1959189ddf
Nathan Fenner <nfenneremail@gmail.com>
2015-03-18 00:33:59 -0400

    parseStatement supports (empty) declarations

--- src/Parse/Statement.hs
+++ src/Parse/Statement.hs
@@ -3,10 +3,12 @@ module Parse.Statement where
 
 import Parse.Core
 import Parse.Expression
+import Parse.Type
+import Lex
 import Location
 
 data StatementForm
-	= Declare (Locate String) (Maybe Expression)
+	= Declare Type (Locate String) (Maybe Expression)
 	| Assign Expression Expression
 	| Perform Expression
 	| If Expression [Statement] [Statement]
@@ -16,4 +18,15 @@ data StatementForm
 	deriving Show
 
 type Statement = Locate StatementForm
+
+parseDeclare :: Parse Statement
+parseDeclare = do
+	varAt <- expect (TSpecial "var") (Message $ "expected `var` to begin variable declaration")
+	-- next there's a type
+	Locate typeAt varType <- parseType
+	expect (TSpecial ":") (Message $ "expected `:` to follow variable declaration type starting at " ++ displayLocation typeAt)
+	Locate nameAt name <- expectName $ Message $ "expected name to follow `:` in type declaration beginning at " ++ displayLocation varAt
+	expect (TSpecial ";") $ Message $ "expected semicolon to follow declaration of variable `" ++ name ++ "` at " ++ displayLocation nameAt ++ " (declaration begins at " ++ displayLocation varAt ++ ")"
+	return $ Locate varAt $ Declare (Locate typeAt varType) (Locate nameAt name) Nothing
+
 
