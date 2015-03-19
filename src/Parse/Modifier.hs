a763bbea2ee5b94f3b1b581cbf02232f8a055ea2
Nathan Fenner <nfenneremail@gmail.com>
2015-03-19 18:49:56 -0400

    modifier

---
+++ src/Parse/Modifier.hs
@@ -0,0 +1,20 @@
+
+module Parse.Modifier where
+import Parse.Core
+import Lex
+
+data Modifier
+	= None
+	| Mutable
+	| Alterable
+	| Final
+	deriving (Show, Eq)
+
+parseModifier :: Parse Modifier
+parseModifier = do
+	next <- peekMaybe
+	case next of
+		Just (Locate _ (TSpecial "mut")) -> advance 1 >> return Mutable
+		Just (Locate _ (TSpecial "alt")) -> advance 1 >> return Alterable
+		Just (Locate _ (TSpecial "fin")) -> advance 1 >> return Final
+		_ -> return None
