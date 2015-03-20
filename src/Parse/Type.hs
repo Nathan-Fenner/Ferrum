86f059a1826a6668b59a56e5d762167e3dc9b721
Nathan Fenner <nfenneremail@gmail.com>
2015-03-20 00:00:38 -0400

    Types include locations

--- src/Parse/Type.hs
+++ src/Parse/Type.hs
@@ -5,33 +5,29 @@ import Parse.Core
 import Lex
 import Location
 
-data TypeField = TypeField String [Type] deriving Show
+data TypeField = TypeField { typeName :: Name, typeArguments :: [Type] } deriving Show
 
 type Type = Locate TypeField
 
 parseType :: Parse Type
 parseType = do
-	n <- peekMaybe
-	case n of
-		Just (Locate nameAt (TWord name)) -> do
-			advance 1 -- to skip the name
-			open <- peekMaybe
-			case open of
-				Just (Locate openAt (TSpecial "[")) -> do
-					advance 1 -- skip the "["
-					first <- parseType
-					rest <- manyWhile
-						(do
-							comma <- peekMaybe
-							case comma of
-								Just (Locate _commaAt (TSpecial ",")) -> do
-									advance 1 -- skip the comma
-									return True
-								_ -> return False
-						)
-						parseType
-					expect (TSpecial "]") (Message $ "expected `]` to close `[` at " ++ displayLocation openAt)
-					return $ Locate nameAt $ TypeField name (first : rest)
-					
-				_ -> return $ Locate nameAt $ TypeField name [] -- no args
-		_ -> crash $ Message "Expected type name"
+	Locate nameAt name <- expectName $ Message "expected type name"
+	open <- peekMaybe
+	case open of
+		Just (Locate openAt (TSpecial "[")) -> do
+			advance 1 -- skip the "["
+			first <- parseType
+			rest <- manyWhile
+				(do
+					comma <- peekMaybe
+					case comma of
+						Just (Locate _commaAt (TSpecial ",")) -> do
+							advance 1 -- skip the comma
+							return True
+						_ -> return False
+				)
+				parseType
+			expect (TSpecial "]") (Message $ "expected `]` to close `[` at " ++ displayLocation openAt)
+			return $ Locate nameAt $ TypeField (Locate nameAt name) (first : rest)
+			
+		_ -> return $ Locate nameAt $ TypeField (Locate nameAt name) [] -- no args
