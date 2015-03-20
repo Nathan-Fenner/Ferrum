555a3bd6a9a99549aa13676e6240d1eb758cb8bf
Nathan Fenner <nfenneremail@gmail.com>
2015-03-20 19:45:27 -0400

    fancy (>.>) operator for verification

--- src/Verify.hs
+++ src/Verify.hs
@@ -10,3 +10,7 @@ type Verify a = Either (Locate Message) a
 
 (>.>) :: Monad m => (t -> m a) -> (t -> m b) -> (t -> m b)
 (f >.> g) x = f x >> g x
+
+(>>=:) :: Monad m => m a -> [a -> m a] -> m a
+e >>=: [] = e
+e >>=: (c : cs) = (e >>= c) >>=: cs
