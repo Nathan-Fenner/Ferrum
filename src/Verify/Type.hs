99f4e74fbdad13c7f3d1d08a8dc9383cdfcaa97f
Nathan Fenner <nfenneremail@gmail.com>
2015-03-20 20:20:00 -0400

    Arity evaluation for Verify.Type
    Arity data for making sure generics and such are used in a consistent
    manner.

--- src/Verify/Type.hs
+++ src/Verify/Type.hs
@@ -11,3 +11,33 @@ verifyTypeForm typeField = formed (value $ typeName $ value typeField) >> mapM_ 
 	formed name@(c:_)
 		|c `elem` ['A'..'Z'] ++ "#" = return ()
 		|otherwise = Left $ Locate (at typeField) $ Message $ "type `" ++ name ++ "` begins with non-capital (and not `#`) letter `" ++ [c] ++ "`"
+
+type ArityFact = (String, Locate Int)
+
+
+
+makeArity :: Name -> Int -> ArityFact
+makeArity name arity = (value name, Locate (at name) arity)
+
+arityFromFact :: ArityFact -> String -> Maybe (Locate Int)
+arityFromFact fact name
+	|fst fact == name = Just $ snd fact
+	|otherwise = Nothing
+
+arityFromFacts :: [ArityFact] -> String -> Maybe (Locate Int)
+arityFromFacts facts name = case [val' | (name', val') <- facts, name == name'] of
+	[] -> Nothing
+	(val : _) -> Just $ val
+
+addArity :: [ArityFact] -> ArityFact -> Verify [ArityFact]
+addArity facts (name, Locate newAt newArity) = case arityFromFacts facts name of
+	Nothing -> return $ (name, Locate newAt newArity) : facts
+	Just (Locate oldAt oldArity) -> if newArity == oldArity
+		then
+			return facts
+		else
+			Left $ Locate newAt $ Message $ "arity of `" ++ name ++ "` (" ++ show newArity ++ ") is inconsistent with its arity (" ++ show oldArity ++ ") at " ++ displayLocation oldAt
+
+unifyArities :: [ArityFact] -> [ArityFact] -> Verify [ArityFact]
+unifyArities [] ys = return ys
+unifyArities (x : xs) ys = addArity ys x >>= unifyArities xs
