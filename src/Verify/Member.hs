e59d615adf9eaf23117e1cd0591ea0cd0a7ef68c
Nathan Fenner <nfenneremail@gmail.com>
2015-03-20 22:48:33 -0400

    methods now descend into statements

--- src/Verify/Member.hs
+++ src/Verify/Member.hs
@@ -2,10 +2,18 @@ module Verify.Member where
 
 import Verify
 import Verify.Type
+import Verify.Statement
 import Parse.Member
+
 
 verifyMemberGenericArity :: Member -> Verify [ArityFact]
 verifyMemberGenericArity member = case memberValue member of
 	field @ Field {} -> concrete $ fieldType field
-	method @ Method {} -> unifyVerifyArity $ map concrete $ methodReturnType method : map fst (methodArguments method)
-	constructor @ Constructor {} -> unifyVerifyArity $ map (concrete . fst) $ constructorArguments constructor
+	method @ Method {} -> unifyVerifyArity $
+		(map verifyStatementGenericArity $ methodBody method)
+		++
+		(map concrete $ methodReturnType method : map fst (methodArguments method))
+	constructor @ Constructor {} -> unifyVerifyArity $
+		(map verifyStatementGenericArity $ constructorBody constructor)
+		++
+		(map (concrete . fst) $ constructorArguments constructor)
