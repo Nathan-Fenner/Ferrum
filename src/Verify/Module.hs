eb2a33154f4004a65fb563ad2a4731f2bf8679f6
Nathan Fenner <nfenneremail@gmail.com>
2015-03-28 04:12:20 -0400

    more helpful error values for duplicated classes
    Now, we print the locations of all of these classes, rather than only
    the first.

--- src/Verify/Module.hs
+++ src/Verify/Module.hs
@@ -12,8 +12,17 @@ verifyModuleClassesUnique :: Module -> Verify ()
 verifyModuleClassesUnique (Module _name classes) = unique $ map className classes where
 	unique [] = return ()
 	unique (n : ns)
-		|value n `elem` map value ns = Left $ Locate (at n) $ Message $ "Class name `" ++ value n ++ "` is not unique"
+		|value n `elem` map value ns = let chosen = filter (\c -> value c == value n) (n:ns) in
+			Left $ Locate (at n) $ Message $
+			"The class name `" ++ value n ++ "` is repeated at " ++ nice (map (displayLocation . at) chosen)
 		|otherwise = unique ns
+		where
+		nice [] = ""
+		nice [a] = a
+		nice [a,b] = a ++ " and " ++ b
+		-- we achieve the Oxford comma this way
+		nice [a,b,c] = a ++ ", " ++ b ++ ", and " ++ c
+		nice (x:xs) = x ++ ", " ++ nice xs
 
 verifyModuleClasses :: Module -> Verify ()
 verifyModuleClasses (Module _name classes) = mapM_ verifyClass classes
