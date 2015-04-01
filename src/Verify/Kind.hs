d1a6f6fd6e8c0787dd9e63617ed70a60db9e3f0a
Nathan Fenner <nfenneremail@gmail.com>
2015-03-31 23:18:35 -0400

    Created "kindOfClass"
    Given a class object, determines its kind by either reporting what it
    states to be true, or inferring it from the number of its generic
    parameters

--- src/Verify/Kind.hs
+++ src/Verify/Kind.hs
@@ -4,3 +4,8 @@ module Verify.Kind where
 import Syntax.Kind
 import Syntax.Class
 
+kindOfClass :: Class -> Kind
+kindOfClass c = case classKind c of
+	Nothing -> simpleKind (length classGeneric)
+	Just k -> k
+
