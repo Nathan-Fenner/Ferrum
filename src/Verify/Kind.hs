b053f7881f797f3231f1d0baac13abfd1118653e
Nathan Fenner <nfenneremail@gmail.com>
2015-04-02 17:31:40 -0400

    simpleModuleKindCheck

--- src/Verify/Kind.hs
+++ src/Verify/Kind.hs
@@ -4,6 +4,7 @@ module Verify.Kind where
 import Verify
 import Syntax.Kind
 import Syntax.Class
+import Syntax.Module
 import Location
 import Message
 
@@ -17,3 +18,7 @@ simpleClassKindCheck c = case
 	kindArity (kindOfClass c) == length (classGeneric c) of
 		True -> return ()
 		False -> Left $ Locate (at $ className c) $ Message $ "class `" ++ value (className c) ++ "` has incompatible kind `(" ++ niceKind (kindOfClass c) ++ ")` with its number of formal parameters (" ++ (show $ length $ classGeneric c) ++ ") which would predict a kind of the form `(" ++ (exampleKind $ length $ classGeneric c) ++ ")`"
+
+simpleModuleKindCheck :: Module -> Verify ()
+simpleModuleKindCheck (Module _ classes) = mapM_ simpleClassKindCheck classes
+
