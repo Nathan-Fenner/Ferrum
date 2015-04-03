fc5714d2b8a0664676097e9e834f1e2128bf2f17
Nathan Fenner <nfenneremail@gmail.com>
2015-04-03 01:53:23 -0400

    verify module kind (without dependencies)

--- src/Verify/Kind.hs
+++ src/Verify/Kind.hs
@@ -75,3 +75,20 @@ verifyClassKind known c@(Class { classMembers = members, classGeneric = generics
 	let genericKinds = zipWith (,) (map value generics) (fst $ uncurryKind $ kindOfClass c)
 	let totalList = genericKinds ++ known
 	mapM_ (verifyMemberKind totalList) members
+
+verifyModuleKind :: Module -> Verify ()
+verifyModuleKind m@Module{ modClasses = classes } = do
+	-- first, check that everything in the module has an appropriate kind
+	-- in particular, that their generic arguments match their explicit kind
+	-- if this explicit kind is stated
+	simpleModuleKindCheck m
+	-- now we can assume that they're correct and consistent
+	let internal = map (\c -> (value $ className c, kindOfClass c)) classes
+
+	-- TODO: actually get the REAL external (requires loading all imported modules)
+	let external = [("Int",Concrete),("Maybe",Arrow Concrete Concrete)]
+	
+	let known = internal ++ external
+	-- internals must be checked too
+	mapM_ (verifyClassKind known) classes
+	return ()
