ba4ad9bb2966ad412c59a90a59696fd96e1837a0
Nathan Fenner <nfenneremail@gmail.com>
2015-04-06 01:40:07 -0400

    Typechecker skeleton

--- src/Verify/Type.hs
+++ src/Verify/Type.hs
@@ -1,7 +1,11 @@
 
 module Verify.Type where
 
+import Verify
 import Syntax.Type
+import Syntax.Module
+import Syntax.Class
+import Syntax.Member
 import Location
 
 relabelType :: String -> String -> Type -> Type
@@ -11,3 +15,13 @@ relabelType from to given = Type
 	where
 	newName = Locate (at givenName) to
 	givenName = typeName $ given
+
+typeCheckModule :: Module -> Verify ()
+typeCheckModule m = mapM_ (typeCheckClass $ modClasses m) $ modClasses m
+
+typeCheckClass :: [Class] -> Class -> Verify ()
+typeCheckClass classes c = mapM_ (typeCheckMember classes) (classMembers c)
+
+typeCheckMember :: [Class] -> Member -> Verify ()
+typeCheckMember = undefined
+
