259f44796a7b587d924d49a7d9af8c97ec9ab9d2
Nathan Fenner <nfenneremail@gmail.com>
2015-04-24 18:12:19 -0400

    Pull member verifier out of type verifier

--- src/Verify/Type.hs
+++ src/Verify/Type.hs
@@ -8,21 +8,10 @@ import Syntax.Class
 import Syntax.Member
 import Verify.Type.Environ
 import Verify.Type.Statement
+import Verify.Type.Member
 import Location
 
--- it doesn't produce any new meaningful stuffs
--- so just operate on the member!
-environMember :: Member -> Environ a -> Verify ()
-environMember Member { memberValue = member } env = go member where
-	go Method { methodReturnType = returnType, methodArguments = args, methodBody = body } = do
-		let env' = env { myReturn = returnType, scope = args ++ scope env }
-		_ <- environBlock body env'
-		return ()
-	go Constructor { constructorArguments = args, constructorBody = body } = do
-		let env' = env { myReturn = voidType, scope = args ++ scope env }
-		_ <- environBlock body env'
-		return ()
-	go Field {} = return () -- nothing to check here
+
 
 environClass :: Class -> Environ a -> Verify ()
 environClass Class { className = name, classGeneric = generic, classMembers = members } env = do
---
+++ src/Verify/Type/Member.hs
@@ -0,0 +1,22 @@
+
+module Verify.Type.Member where
+
+import Verify
+import Verify.Type.Environ
+import Verify.Type.Statement
+import Syntax.Type
+import Syntax.Member
+
+-- it doesn't produce any new meaningful stuffs
+-- so just operate on the member!
+environMember :: Member -> Environ a -> Verify ()
+environMember Member { memberValue = member } env = go member where
+	go Method { methodReturnType = returnType, methodArguments = args, methodBody = body } = do
+		let env' = env { myReturn = returnType, scope = args ++ scope env }
+		_ <- environBlock body env'
+		return ()
+	go Constructor { constructorArguments = args, constructorBody = body } = do
+		let env' = env { myReturn = voidType, scope = args ++ scope env }
+		_ <- environBlock body env'
+		return ()
+	go Field {} = return () -- nothing to check here
