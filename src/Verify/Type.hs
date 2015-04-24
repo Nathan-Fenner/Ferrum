80bfe03a9ac7cbede6c69cbf9368d1f142a01416
Nathan Fenner <nfenneremail@gmail.com>
2015-04-24 18:10:56 -0400

    Pull statement verifier out of type verifier

--- src/Verify/Type.hs
+++ src/Verify/Type.hs
@@ -1,63 +1,14 @@
 
 module Verify.Type where
 
-import Message
 import Verify
 import Syntax.Type
 import Syntax.Module
 import Syntax.Class
 import Syntax.Member
-import Syntax.Statement
-import Verify.Type.Expression
 import Verify.Type.Environ
+import Verify.Type.Statement
 import Location
-
-
-
-
-environStatement :: Statement -> Environ a -> Verify (Environ a)
-environStatement Locate{at=loc, value=statement} env = go statement where
-	go Declare { declarationType = varType, declarationName = varName, declarationExpression = expr} = do
-		let env' = environSetType varType varName env -- done this way, they shadow without being checked
-		case expr of
-			Nothing -> return ()
-			Just e -> do
-				eType <- environExpressionType e env'
-				assert (eType == varType) $ Locate (at e) $ Message $ "While declaring variable `" ++ value varName ++ "`, expression given has wrong type; expected `" ++ prettyType varType ++ "` but assigned `" ++ prettyType eType ++ "`"
-		return env'
-	go Assign { assignLeft = leftExpression, assignRight = rightExpression } = do
-		leftType <- environExpressionType leftExpression env
-		rightType <- environExpressionType rightExpression env
-		assert (leftType == rightType) $ Locate loc $ Message $ "Assignment types do not match; left has type `" ++ prettyType leftType ++ "` while right has type `" ++ prettyType rightType ++ "`"
-		return env
-	go Perform { performExpression = expression } = do -- just check expression typechecks
-		_ <- environExpressionType expression env
-		return env
-	go If { ifCondition = cond, ifThenBody = thenBody, ifElseBody = elseBody } = do
-		condType <- environExpressionType cond env
-		assert (condType == boolType) $ Locate loc $ Message $ "if statement must have condition of type `Bool` but is given condition of type `" ++ prettyType condType ++ "` instead"
-		_ <- environBlock thenBody env
-		_ <- environBlock elseBody env
-		return env
-	go While { whileCondition = cond, whileBody = body } = do
-		condType <- environExpressionType cond env
-		assert (condType == boolType) $ Locate loc $ Message $ "while statement must have condition of type `Bool` but is given condition of type `" ++ prettyType condType ++ "` instead"
-		_ <- environBlock body env
-		return env
-	go Break = return env
-	go Return{returnExpression = Nothing} = do
-		assert (myReturn env == voidType ) $ Locate loc $ Message $ "void methods cannot return values"
-		return env
-	go Return{returnExpression = Just e} = do
-		eType <- environExpressionType e env
-		assert (myReturn env == eType) $ Locate loc $ Message $ "method expected to return `" ++ prettyType (myReturn env) ++ "` but returned expression of type `" ++ prettyType eType ++ "` instead"
-		return env
-
-environBlock :: [Statement] -> Environ a -> Verify (Environ a)
-environBlock [] env = return env
-environBlock (s : ss) env = do
-	env' <- environStatement s env
-	environBlock ss env'
 
 -- it doesn't produce any new meaningful stuffs
 -- so just operate on the member!
---
+++ src/Verify/Type/Statement.hs
@@ -0,0 +1,55 @@
+
+module Verify.Type.Statement where
+
+import Message
+import Location
+import Verify
+import Verify.Type.Environ
+import Verify.Type.Expression
+import Syntax.Type
+import Syntax.Statement
+
+
+environStatement :: Statement -> Environ a -> Verify (Environ a)
+environStatement Locate{at=loc, value=statement} env = go statement where
+	go Declare { declarationType = varType, declarationName = varName, declarationExpression = expr} = do
+		let env' = environSetType varType varName env -- done this way, they shadow without being checked
+		case expr of
+			Nothing -> return ()
+			Just e -> do
+				eType <- environExpressionType e env'
+				assert (eType == varType) $ Locate (at e) $ Message $ "While declaring variable `" ++ value varName ++ "`, expression given has wrong type; expected `" ++ prettyType varType ++ "` but assigned `" ++ prettyType eType ++ "`"
+		return env'
+	go Assign { assignLeft = leftExpression, assignRight = rightExpression } = do
+		leftType <- environExpressionType leftExpression env
+		rightType <- environExpressionType rightExpression env
+		assert (leftType == rightType) $ Locate loc $ Message $ "Assignment types do not match; left has type `" ++ prettyType leftType ++ "` while right has type `" ++ prettyType rightType ++ "`"
+		return env
+	go Perform { performExpression = expression } = do -- just check expression typechecks
+		_ <- environExpressionType expression env
+		return env
+	go If { ifCondition = cond, ifThenBody = thenBody, ifElseBody = elseBody } = do
+		condType <- environExpressionType cond env
+		assert (condType == boolType) $ Locate loc $ Message $ "if statement must have condition of type `Bool` but is given condition of type `" ++ prettyType condType ++ "` instead"
+		_ <- environBlock thenBody env
+		_ <- environBlock elseBody env
+		return env
+	go While { whileCondition = cond, whileBody = body } = do
+		condType <- environExpressionType cond env
+		assert (condType == boolType) $ Locate loc $ Message $ "while statement must have condition of type `Bool` but is given condition of type `" ++ prettyType condType ++ "` instead"
+		_ <- environBlock body env
+		return env
+	go Break = return env
+	go Return{returnExpression = Nothing} = do
+		assert (myReturn env == voidType ) $ Locate loc $ Message $ "void methods cannot return values"
+		return env
+	go Return{returnExpression = Just e} = do
+		eType <- environExpressionType e env
+		assert (myReturn env == eType) $ Locate loc $ Message $ "method expected to return `" ++ prettyType (myReturn env) ++ "` but returned expression of type `" ++ prettyType eType ++ "` instead"
+		return env
+
+environBlock :: [Statement] -> Environ a -> Verify (Environ a)
+environBlock [] env = return env
+environBlock (s : ss) env = do
+	env' <- environStatement s env
+	environBlock ss env'
