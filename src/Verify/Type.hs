81cd6e59f999b1591d7d0b869305194760648f27
Nathan Fenner <nfenneremail@gmail.com>
2015-04-24 02:35:44 -0400

    many checks for operators

--- src/Verify/Type.hs
+++ src/Verify/Type.hs
@@ -114,18 +114,28 @@ environExpressionType expr env = case value expr of
 		verifyOperator op leftType rightType
 	_ -> undefined
 
+verifyUnary :: Name -> Type -> Verify Type
+verifyUnary Locate{at=loc, value=op} thing
+	|op == "not" = (assert (thing == boolType) $ Locate loc $ Message $ "operator to prefix `not` must be of type `Bool` but is of type `" ++ prettyType thing ++ "`") >> return boolType
+	|op == "-" = (assert (thing == intType) $ Locate loc $ Message $ "operator to prefix `-` must be of type `Int` but is of type `" ++ prettyType thing ++ "`") >> return intType
+verifyUnary _ _ = error "cannot handle that prefix operator yet"
 
 verifyOperator :: Name -> Type -> Type -> Verify Type
 verifyOperator Locate{at=loc, value=op} left right
-	|op `elem` ["+","-","*","/","%"] = assertBothReturn intType
-	|op == "++" = assertBothReturn stringType
-	|op `elem` ["and","or"] = assertBothReturn boolType
+	|op `elem` ["+","-","*","/","%"] = assertBoth intType >> return intType
+	|op `elem` [">","<",">=","<="] = assertBoth intType >> return boolType
+	|op `elem` ["==","~="] = assertSame >> return boolType
+	|op == "++" = assertBoth stringType >> return stringType
+	|op `elem` ["and","or"] = assertBoth boolType >> return boolType
+
 	where
-	assertBothReturn t = do
-		assert (left == t) $ Locate loc $ Message $ "left operand to `" ++ op ++ "` does not have type " ++ prettyType t
-		assert (right == t) $ Locate loc $ Message $ "right operand to `" ++ op ++ "` does not have type " ++ prettyType t
-		return t
-verifyOperator _ _ _ = undefined
+	assertSame = do
+		assert (left == right) $ Locate loc $ Message $ "left and right operands to `" ++ op ++ "` have different types, `" ++ prettyType left ++ "` and `" ++ prettyType right ++ "`"
+	assertBoth t = do
+		assert (left == t) $ Locate loc $ Message $ "left operand to `" ++ op ++ "` does not have type `" ++ prettyType t ++ "` but instead had type `" ++ prettyType left ++ "`"
+		assert (right == t) $ Locate loc $ Message $ "right operand to `" ++ op ++ "` does not have type `" ++ prettyType t ++ "` but instead has type `" ++ prettyType right ++ "`"
+
+verifyOperator _ _ _ = error "cannot handle that operator yet"
 
 
 
